efc2186d52485c4465d8caf2cc1f3a8d83cae37a
lab lab <lab@labinfo.is.escuelaing.edu.co>
2024-08-22 15:25:03 -0500

    Más parámetros

--- Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -9,11 +9,11 @@ public class App
     public static void main( String[] args )
     {   
         if(args.length != 0) {
-            String name = "";
+            /*String name = "";
             for(int i=0;i<args.length;i++){
                 name+=args[i];
-            }
-            System.out.println("Hello "+name);
+            }*/
+            System.out.println("Hello "+String.join(", ",args));
         }else{
             System.out.println( "Hello World!" );
         }
