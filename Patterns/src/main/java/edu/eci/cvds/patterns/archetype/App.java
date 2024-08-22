49e027a0f106173bec813fa88196507354385e09
lab lab <lab@labinfo.is.escuelaing.edu.co>
2024-08-22 15:14:54 -0500

    Cambios, mensaje personal

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -0,0 +1,22 @@
+package edu.eci.cvds.patterns.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {   
+        if(args.length != 0) {
+            String name = "";
+            for(int i=0;i<args.length;i++){
+                name+=args[i];
+            }
+            System.out.println("Hello "+name);
+        }else{
+            System.out.println( "Hello World!" );
+        }
+
+    }
+}
